396245e4225625498859dbc2d4518121eb0ec245
Andres Parra <andressparrarlst@gmail.com>
2026-02-21 20:39:37 -0500

    fix: problem in one enpoint and add a new enpoint to know the ip in the maquine

--- src/main/java/com/uptc/uptc/UptcController.java
+++ src/main/java/com/uptc/uptc/UptcController.java
@@ -1,5 +1,7 @@
 package com.uptc.uptc;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +9,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class UptcController {
-    
+
     private final UptcUserService userService;
 
     public UptcController(UptcUserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/getAllUsers, version=1")
+    @GetMapping("/getAllUsers")
     public List<UptcUser> getUsers() {
         return userService.getAllUsers();
     }
@@ -26,6 +27,14 @@ public class UptcController {
     public UptcUser createUser(@RequestBody UptcUser uptcUser) {
         return userService.createUser(uptcUser);
     }
-    
-    
+
+    @GetMapping("/ip")
+    public String getServerIp() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            return "IP del servidor: " + ip.getHostAddress();
+        } catch (UnknownHostException e) {
+            return "No se pudo obtener la IP";
+        }
+    }
 }
