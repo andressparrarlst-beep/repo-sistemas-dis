2bd35f03f0cb6c1abe94c762dbcbf65124ec5bf0
Andres Parra <andressparrarlst@gmail.com>
2026-03-18 22:50:33 -0500

    feat: implement ApiResponse class and update UptcController methods to return structured responses

---
+++ src/main/java/com/uptc/uptc/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.uptc.uptc;
+
+  
+
+import lombok.AllArgsConstructor;
+
+import lombok.Getter;
+
+import lombok.NoArgsConstructor;
+
+  
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ApiResponse<T> {
+    
+    private String server;
+    private T data;
+
+}
--- src/main/java/com/uptc/uptc/UptcController.java
+++ src/main/java/com/uptc/uptc/UptcController.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UptcController {
 
+    @Value("${server.id:no especificado}")
+    private String serverId;
+    
     private final UptcUserService userService;
 
     public UptcController(UptcUserService userService) {
@@ -19,22 +23,22 @@ public class UptcController {
     }
 
     @GetMapping("/getAllUsers")
-    public List<UptcUser> getUsers() {
-        return userService.getAllUsers();
+    public ApiResponse<List<UptcUser>> getUsers() {
+        return new ApiResponse<>(serverId,userService.getAllUsers());
     }
 
     @PostMapping("createUser")
-    public UptcUser createUser(@RequestBody UptcUser uptcUser) {
-        return userService.createUser(uptcUser);
+    public ApiResponse<UptcUser> createUser(@RequestBody UptcUser uptcUser) {
+        return new ApiResponse<>(serverId,userService.createUser(uptcUser));
     }
 
     @GetMapping("/ip")
-    public String getServerIp() {
+    public ApiResponse<String> getServerIp() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
-            return "IP del servidor: " + ip.getHostAddress();
+            return new ApiResponse<>(serverId,"IP del servidor: " + ip.getHostAddress());
         } catch (UnknownHostException e) {
-            return "No se pudo obtener la IP";
+            return new ApiResponse<>(serverId,"No se pudo obtener la IP");
         }
     }
 }
