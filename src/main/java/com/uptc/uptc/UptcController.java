c5818ecaf239568356bb762e673fa650c8b6677a
Andres Parra <andressparrarlst@gmail.com>
2026-03-22 16:12:20 -0500

    feature: endpoits to checkout failure

--- src/main/java/com/uptc/uptc/UptcController.java
+++ src/main/java/com/uptc/uptc/UptcController.java
@@ -2,6 +2,7 @@ package com.uptc.uptc;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -15,8 +16,9 @@ public class UptcController {
 
     @Value("${server.id:no especificado}")
     private String serverId;
-    
+
     private final UptcUserService userService;
+    private List<byte[]> memory = new ArrayList<>();
 
     public UptcController(UptcUserService userService) {
         this.userService = userService;
@@ -24,21 +26,36 @@ public class UptcController {
 
     @GetMapping("/getAllUsers")
     public ApiResponse<List<UptcUser>> getUsers() {
-        return new ApiResponse<>(serverId,userService.getAllUsers());
+        return new ApiResponse<>(serverId, userService.getAllUsers());
     }
 
     @PostMapping("createUser")
     public ApiResponse<UptcUser> createUser(@RequestBody UptcUser uptcUser) {
-        return new ApiResponse<>(serverId,userService.createUser(uptcUser));
+        return new ApiResponse<>(serverId, userService.createUser(uptcUser));
     }
 
     @GetMapping("/ip")
     public ApiResponse<String> getServerIp() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
-            return new ApiResponse<>(serverId,"IP del servidor: " + ip.getHostAddress());
+            return new ApiResponse<>(serverId, "IP del servidor: " + ip.getHostAddress());
         } catch (UnknownHostException e) {
-            return new ApiResponse<>(serverId,"No se pudo obtener la IP");
+            return new ApiResponse<>(serverId, "No se pudo obtener la IP");
         }
+    }
+
+    @GetMapping("/cpu")
+    public String cpu() {
+
+        while (true) {
+            Math.sqrt(Math.random());
+        }
+
+    }
+
+    @GetMapping("/memory")
+    public String memoryLeak() {
+        memory.add(new byte[1024 * 1024]); // 1 MB por request
+        return "Memoria consumida: " + memory.size() + " MB";
     }
 }
